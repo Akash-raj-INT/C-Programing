858704b0949383931c6b758315ef19e3d35aa31f
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-02-09 21:08:34 +0530

    Add files via upload

---
+++ first code/Untitled5.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+	int rollno;
+	char name[10];
+	
+	printf("Enter your name= ");
+	sacnf("%d" ,&rollno);
+	printf("Enter your name= ");
+	scanf("%s", &name)
+	prinf("in your roll number= %d,rollno");
+	print("in your name = %s", name);
+	return 0;
+}
