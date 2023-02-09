20725795a7db221bb367644672237495f3a4db39
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-02-09 21:07:31 +0530

    Add files via upload

---
+++ first code/Untitled2.cpp
@@ -0,0 +1,17 @@
+//wap to print the details of a student like name, registration no. , roll no. ,city, state, contact no,
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	printf("\nName = Akash Raj\n");
+	printf("\nRegistration no = 12220043");
+	printf("\nRoll no = 28");
+	printf("\nCity = Patna");
+	printf("State = Bihar");
+	printf("Contact no = 8271xxxxxx");
+	printf("CGPA = 5" );
+	
+	return 0;
+}
