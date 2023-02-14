542e4141af6932c5f60c975b266bd17dea45de31
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-02-14 23:13:04 +0530

    Add files via upload

---
+++ Operators/operator1.cpp
@@ -0,0 +1,25 @@
+//write a program find weither the no. by user is smallest or greater out of two variable
+
+#include <stdio.h>
+#include <conic.h>
+
+int main()
+   {
+   	int a,b;
+   	printf("\n enter a no. = ");
+    scanf("%d",&a);
+    
+    printf("\n enter b no. = ");
+    scanf("%d",&b);
+    
+    if (a>b){
+    	printf("a is greater then b");
+    	}
+else
+  {
+  	printf("a is smaller then b");
+  }
+  	return 0 ;
+	  
+  }
+   
