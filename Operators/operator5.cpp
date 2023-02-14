dc3798819ebe32e9a11276343a57ff6f0178db61
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-02-14 23:14:27 +0530

    Add files via upload

---
+++ Operators/operator5.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int a,b;
+	print("enter the value :", a);
+	print("enter the value :", b)
+	printf("a= %d, b=%d\n", a,b);
+	printf("a&b =%d\n",a&b);
+	printf("a|b = %d\n", a|b);
+	printf("a^b = %d\n", a^b);
+	printf("~a= %d\n", a= ~b);
+	printf("b<<1 = %d\n",b <<1);
+	printf("b>>1 = %d\n", b>>1);
+	return 0;
+}
