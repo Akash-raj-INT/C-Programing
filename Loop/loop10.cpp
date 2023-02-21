edcf51d9cc33683672c762a2c61b79406fa2dae4
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-02-21 23:43:45 +0530

    Add files via upload

---
+++ Loop/loop10.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int n,i,sum=0;
+	printf("Enter the value: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		sum=sum+i;
+	}
+	printf("the sum=%d",sum);
+	return 0;
+}
