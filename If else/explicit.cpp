addddcd79cab0ac40f2f990ff41d395aa1a1f15d
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-03-05 01:27:59 +0530

    Add files via upload

---
+++ If else/explicit.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int average, total;
+	printf("Enter no: ");
+	scanf("%f"& total;)
+	float counter;
+	
+	average = total/(int)counter;
+	printf("Enter the no: ");
+	scanf("%d",&average);
+	
+}
