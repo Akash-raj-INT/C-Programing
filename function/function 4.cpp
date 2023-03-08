403b6ece44fd975cda1b1fddb3517b26204b51be
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-03-08 22:15:30 +0530

    Add files via upload

---
+++ function/function 4.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int x,y;
+	printf("Enter the no of x: ");
+	scanf("%d",&x);
+	printf("Enter the no of y: ");
+	scanf("%d",&y);
+    
+	printf("the square is=%f",sqrt(x));	
+	printf("\nthe expo is=%f",exp(x));
+	printf("\nthe log is=%f",log(x));
+	printf("\nthe log10 is=%f",log(x));
+	printf("\nthe fabs is=%f",fabs(x));
+	printf("\nthe ceil is=%f",ceil(x));
+	printf("\nthe floor is=%f",floor(x));
+	printf("\nthe pow is =%f",pow(x,y));
+	pri
+}
