e91bd2e59b685904939b7c2489b2876df293f0c2
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-04-20 12:53:38 +0530

    Add files via upload

---
+++ array/Untitled7.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a[3][3],i,j;
+	for(i=0; i<3;i++for)
+	{
+		for(j=0; j<3;j++)
+		printf("Enter the value of a[%d][%d]:",i,j);
+		scanf("%d", &a[i][j]);
+		
+	}
+}
